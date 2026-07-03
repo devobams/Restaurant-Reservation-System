b7c812947f5681bc9ad391446a40e5caa3156c31
ojekab4u <ojekab2580@gmail.com>
2026-07-03 10:57:30 +0100

    createReservation route written

---
+++ src/routes/reservation.routes.js
@@ -0,0 +1,7 @@
+import express from "express"
+import * as reservationController from "../controllers/reservation.controller.js"
+
+const router = express.Router();
+
+
+router.post('/', reservationController.createReservation);

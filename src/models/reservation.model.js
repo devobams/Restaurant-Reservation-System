a7f3a385ac1cca30d8507872d8982fce51051824
devobams <devobams@gmail.com>
2026-07-03 23:30:27 +0100

    updated reservation mode, import utils
    created function to generate randomIds using js GUID crypo module
    
    created a function to randomly assign tables for reservations

--- src/models/reservation.model.js
+++ src/models/reservation.model.js
@@ -1,16 +1,28 @@
-class Reservation{
-    constructor({fullName, phoneNumber, date, time, numberOfGuest, tableNumber, status
+import { generateId } from "../utils/generateId.js";
+import { generateTableNumber } from "../utils/generateTableNumber.js";
+export default class Reservation{
+    constructor({fullName, phoneNumber, date, time, numberOfGuest, status
     }){
         this.reservationId = generateId();
         this.fullName = fullName;
-        this.phoneNumber = phoneNumber; 
+        this.phoneNumber = phoneNumber;
         this.date = date;
         this.time = time;
         this.numberOfGuest = numberOfGuest;
-        this.tableNumber = tableNumber;
+        this.tableNumber = generateTableNumber(); // randomly assign a table number between 1 and 10
         this.status = status ?? "pending";
-        this.createdAt = new Date().toISOString;
-        this.updatedAt = new Date().toISOString;
+        this.createdAt = new Date().toISOString();
+        this.updatedAt = new Date().toDateString();
     }
 }
-module.exports = Reservation
+
+// const sampleReservation = new Reservation({
+//     fullName: "John Doe",
+//     phoneNumber: "123-456-7890",
+//     date: "2023-07-15",
+//     time: "19:00",
+//     numberOfGuest: 4,
+//     status: "pending"
+// });
+
+// console.log(sampleReservation);

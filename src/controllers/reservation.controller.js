e24d5acbb769137db222b8e0e5825e027da1024d
devobams <devobams@gmail.com>
2026-07-06 21:55:48 +0100

    implement-updateReservation-controller

--- src/controllers/reservation.controller.js
+++ src/controllers/reservation.controller.js
@@ -21,6 +21,23 @@ export async function getReservations(req, res) {
 
 export function getReservationById(req, res) {}
 
-export function updateReservation(req, res) {}
+export async function updateReservation(req, res) {
+  // steps to implement logic
+  // 1. Get the reservation id from the request params
+  const { id } = req.params;
+  // 2. Get the data from the request body 
+  const reservationData = req.body;
+  // validate the incoming data before updating the reservation
+  const validate = validateReservation(reservationData);
+  if (!validate.valid) {
+    const error = new AppError(validate.message);
+    error.statusCode = 400;
+    throw error;
+  }
+  // 3. Call the service function to update the reservation
+  const updatedReservation = await reservationService.updateReservation(id, reservationData);
+  // 4. Return the updated reservation in the response
+  res.status(200).json(updatedReservation);
+}
 
 export function deleteReservation(req, res) {}

60f8d04d9049418c7dc0beeea2f84c160d72a090
Osiebe Judith <osiebejudith2022@gmail.com>
2026-07-07 21:35:23 +0100

    Initialize reservation status in model

--- src/models/reservation.model.js
+++ src/models/reservation.model.js
@@ -1,6 +1,6 @@
 import { generateId } from "../utils/generateId.js";
 export default class Reservation{
-    constructor({fullName, phoneNumber, date, time, numberOfGuest, tableNumber,
+    constructor({fullName, phoneNumber, date, time, numberOfGuest,
     }){
         this.reservationId = generateId();
         this.fullName = fullName;
@@ -9,7 +9,7 @@ export default class Reservation{
         this.time = time;
         this.numberOfGuest = numberOfGuest;
         this.tableNumber = tableNumber; // real available table, assigned by the service
-        this.status = null; // e.g. 'CONFIRMED' or 'CANCELLED', decided by the service
+        this.status = status; // e.g. 'CONFIRMED' or 'CANCELLED', decided by the service
         this.createdAt = new Date().toISOString();
         this.updatedAt = new Date().toISOString();
     }
